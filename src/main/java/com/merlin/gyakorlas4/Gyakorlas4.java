348c989b10aa3ee7823807229dc0ac858c6b7ad5
Kiss Tamas <tamas@1986>
2023-05-24 21:05:34 +0200

    gyakorlas4 első

---
+++ src/main/java/com/merlin/gyakorlas4/Gyakorlas4.java
@@ -0,0 +1,17 @@
+package com.merlin.gyakorlas4;
+
+import java.util.ArrayList;
+
+public class Gyakorlas4 
+{
+    //ArrayList<Product> a= new ArrayList<>();
+   public static void main(String[] args)
+    {
+     /*Product product = new Product();
+       
+       a.add(product);*/
+        
+     
+       
+    }
+}
---
+++ src/main/java/com/merlin/gyakorlas4/Product.java
@@ -0,0 +1,10 @@
+
+package com.merlin.gyakorlas4;
+
+public class Product 
+{
+    private String name;
+    private int price;
+    private int sMoney;
+    private double discount;
+}
